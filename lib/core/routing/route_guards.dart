6ea4c1dd3b0fd44a1359ab1ed9a9f7a92d762d7f
nehal <nhbhuiyan2003@gmail.com>
2026-06-19 03:42:44 +0600

    fix(core): resolve relative import path and riverpod v3 legacy import issues in route guards and auth provider

--- lib/core/routing/route_guards.dart
+++ lib/core/routing/route_guards.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import '../../utils/constants.dart';
+import '../utils/constants.dart';
 import '../providers/auth_provider.dart';
 
 class RouteGuards {
